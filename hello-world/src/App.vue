1c6d585fb4b127bfb83193eba144a3cc91ad654f
nnakano55 <nnakano@ucsc.edu>
2022-02-07 02:57:05 -0800

    initial commit

---
+++ hello-world/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="message"/>
+    <input type="text" v-model="message" />
+    <button @click="alertMessage">Alert</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      message: 'Hello World!'
+    };
+  },
+  methods: {
+    alertMessage(){
+      // could perform some kind of POST function for some kind of webapp
+      alert(this.message);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
